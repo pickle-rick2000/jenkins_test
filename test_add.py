4001abbe63922bad990752ed4c03413057f6e31e
Jhonatan Sela <jhonatansela1@gmail.com>
2021-02-16 11:59:34 +0000

    checking trigger by push, just added def in test_add.py

--- test_add.py
+++ test_add.py
@@ -3,5 +3,8 @@ import mod_add
 def test_add():
         assert mod_add.add(2,2)==4
 
-        def test_add2():
-                assert mod_add.add(3,3)==6
+def test_add2():
+        assert mod_add.add(3,3)==6
+
+def test_add3():
+        assert mod_add.add(5,5)==10
